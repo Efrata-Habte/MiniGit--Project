02d251d58f10d90f208daa0e96510535de74e8db
gelila0913 <gelilasintayehu79@gmail.com>
2025-06-18 23:57:52 +0300

    Add files via upload
    This header declares the sha1 function used for generating SHA-1 hashes of input strings.
    It will be used in the MiniGit system to uniquely identify file contents by hash.

---
+++ hash.cpp
@@ -0,0 +1,13 @@
+#include "hash.h"
+#include <sstream>
+#include <iomanip>
+#include <openssl/sha.h>
+
+std::string sha1(const std::string& data) {
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    SHA1(reinterpret_cast<const unsigned char*>(data.c_str()), data.size(), hash);
+    std::ostringstream oss;
+    for (int i = 0; i < SHA_DIGEST_LENGTH; ++i)
+        oss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    return oss.str();
+}
