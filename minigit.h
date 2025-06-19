112e1754032b2e93bdfb86e7498471d9d2aefdf3
Beimnet-Abdi <beimnet.ugr-8524-16@aau.edu.et>
2025-06-19 03:36:53 -0700

    Add comments to minigit.h

--- minigit.h
+++ minigit.h
@@ -3,12 +3,26 @@
 #include <string>
 
 namespace MiniGit {
-    void init();
+    
+    // Initialize a new MiniGit repo in the current directory
+    void init(); 
+    
+    // Adds a file to staging index 
     void add(const std::string& filename);
+
+    // Commits all staged files with a given message
     void commit(const std::string& message);
+    
+    // Displays the commit hisotry 
     void log();
+
+    // Cretes new branches pointing to the current branch
     void branch(const std::string& branchName);
+
+    // Checks out a branch or a specific commit
     void checkout(const std::string& name);
+
+    // Joins the changes from another branch into the current branch
     void merge(const std::string& branchName);
 }
 
